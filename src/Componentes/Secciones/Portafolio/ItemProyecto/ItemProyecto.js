43aeb8446dbeb9ed1d68bd5c2a3c535dcf2dd930
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2022-08-15 19:32:33 -0300

    Modificaciones Seccion Portafolio-v0.2

--- src/Componentes/Secciones/Portafolio/ItemProyecto/ItemProyecto.js
+++ src/Componentes/Secciones/Portafolio/ItemProyecto/ItemProyecto.js
@@ -6,23 +6,27 @@ import './ItemProyecto.scss'
 
 export default function ItemProyecto({titulo, descripcion, posicionItem, imagenCelular, imagenNavegador, tituloHttp, logo, colorFondo}) {
     return (
-        <div className={`contenedor-ItemProyecto ${posicionItem}`}>
-           <div className='contenedor-detalles-ItemProyecto'>
-                <h1>{titulo}</h1>
-                <p>{descripcion}</p>
-                <button>code</button>
-                <button>demo</button>
-            </div>
-            <div className='contenedor-imagenes-ItemProyecto'> 
-                <Navegador  tituloHttp={tituloHttp} logo={logo} colorFondo={colorFondo}>
-                    <img src={imagenNavegador} alt='img' />        
-                </Navegador>
-                <Celular tituloHttp={tituloHttp} colorFondo={colorFondo}>
-                    <img src={imagenCelular} alt='img' />   
-                </Celular>
+        <div className={`contenedor-ItemProyecto`}>
+            <div className='borde-contenedor-ItemProyecto' />
 
-            </div>
+            <div className={`contenedor-proyecto-ItemProyecto ${posicionItem}`}>
+                <div className='contenedor-imagenes-ItemProyecto'> 
+                    <Navegador  tituloHttp={tituloHttp} logo={logo} colorFondo={colorFondo}>
+                        <img src={imagenNavegador} alt='img' />        
+                    </Navegador>
+                    <Celular tituloHttp={tituloHttp} colorFondo={colorFondo}>
+                        <img src={imagenCelular} alt='img' />   
+                    </Celular>
+                </div>
 
+                <div className='contenedor-detalles-ItemProyecto'>
+                    <h1>{titulo}</h1>
+                    <p>{descripcion}</p>
+                    <button>code</button>
+                    <button>demo</button>
+                </div>
+
+            </div>        
         </div>
     )
 }
